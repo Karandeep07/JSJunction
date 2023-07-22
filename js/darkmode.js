5affb745568fd06df2d35c83a117b6c45acbc556
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2023-07-22 12:31:02 +0530

    fix: Navbar for sm displays

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,16 +1,16 @@
 function myFunction() {
   var element = document.body;
   element.dataset.bsTheme =
-    element.dataset.bsTheme == "light" ? "dark" : "light";
+    element.dataset.bsTheme == "dark" ? "light" : "dark";
 
   const moon = document.getElementById("moon");
   const sun = document.getElementById("sun");
 
-  if (moon.style.zIndex == 1) {
-    moon.style.zIndex = -1;
-    sun.style.zIndex = 1;
-  } else {
-    moon.style.zIndex = 1;
+  if (sun.style.zIndex == 1) {
     sun.style.zIndex = -1;
+    moon.style.zIndex = 1;
+  } else {
+    sun.style.zIndex = 1;
+    moon.style.zIndex = -1;
   }
 }
